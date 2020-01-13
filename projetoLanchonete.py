a24cfd0475af5c0878daa664df93b45026e2c46f
Sergio Campos <sermarcia@hotmail.com>
2020-01-12 22:23:42 -0300

    Criado as funcões cadastrarProdutos e listarProdutos

--- projetoLanchonete.py
+++ projetoLanchonete.py
@@ -11,14 +11,14 @@ conexao = pymysql.connect(
 
 autentico = False
 
-def logarCadastrar():
+def logarCadastrar(decidir):
     usuarioExistente = 0
     autenticado = False
     usuarioMaster = False
 
     if decisao == 1:
-        nome = input('Digite seu Nome: ')
-        senha = input('Digite sua Senha: ')
+        nome = input('Digite seu Nome: \n')
+        senha = input('Digite sua Senha: \n')
 
         for linha in resultado:
             if nome == linha['nome'] and senha == linha['senha']:
@@ -27,16 +27,17 @@ def logarCadastrar():
                 elif linha['nivel'] == 2:
                     usuarioMaster = True
                 autenticado = True
+                break
             else:
                 autenticado = False
 
         if not autenticado:
-            print('email ou senha errados')
+            print('email ou senha errada')
 
     elif decisao == 2:
         print('Faça seu cadastro')
-        nome = input('Digite seu Nome: ')
-        senha = input('Digite sua Senha')
+        nome = input('Digite seu Nome: \n')
+        senha = input('Digite sua Senha: \n')
 
         for linha in resultado:
             if nome == linha['nome'] and senha == linha['senha']:
@@ -52,7 +53,40 @@ def logarCadastrar():
 
     return autenticado, usuarioMaster
 
+def cadastrarProdutos():
+    nome = input('Digite o nome do Produto: \n')
+    ingredientes = input('Digite os ingredientes dos Produtos: \n')
+    grupo = input('Digite o grupo pertencente a este Produto: \n')
+    preco = float(input('Digite o preço do Produto: \n'))
 
+    # INSERIR OS DADOS NO BANCO, E FAZER O TRATAMENTO DE ERROS
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('insert into produtos (nome, ingredientes, grupo, preco) values (%s, %s, %s, %s);', (nome, ingredientes, grupo, preco))
+            conexao.commit()
+            print('Produto cadastrado com sucesso')
+    except:
+        print('Erro ao cadastrar os produtos no banco de dados!!!')
+
+def listarProdutos():
+    # Criar uma lista com os produtos
+    produtos = []
+
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('select * from produtos;')
+            produtosCadastrados = cursor.fetchall()
+    except:
+        print('Erro ao conectar ao banco de dados')
+
+    for i in produtosCadastrados:
+        produtos.append(i)
+
+    if len(produtos) != 0:
+        for i in range(0, len(produtos)):
+            print(produtos[i])
+    else:
+        print('Nenhum Produto Cadastrado...')
 
 while not autentico:
     decisao = int(input('Digite 1 para Logar e 2 para se Cadastrar'))
@@ -64,7 +98,19 @@ while not autentico:
     except:
         print('Erro ao conectar ao Banco de Dados')
 
-    autentico, usuarioSupremo = logarCadastrar()
+    autentico, usuarioSupremo = logarCadastrar(decisao)
 
 if autentico == True:
     print('autenticado...')
+
+    if usuarioSupremo == True:
+        decisaoUsuario = 1
+
+        while decisaoUsuario != 0:
+            decisaoUsuario = int ( input ( 'Digite 0 para sair, 1 para cadastrar produtos, 2 para listar produtos cadastrados' ) )
+
+            if decisaoUsuario == 1:
+                cadastrarProdutos()
+            elif decisaoUsuario == 2:
+                listarProdutos()
+
