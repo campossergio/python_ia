9325d38c95e62155c7bf360c4cfe6400f5c95a4c
Sergio Campos <sermarcia@hotmail.com>
2020-01-17 23:47:01 -0300

    Finalizado o projeto ERP projetoLanchonete.py

--- projetoLanchonete.py
+++ projetoLanchonete.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pymysql.cursors
 
 conexao = pymysql.connect(
@@ -88,6 +89,201 @@ def listarProdutos():
     else:
         print('Nenhum Produto Cadastrado...')
 
+def excluirProdutos():
+    idDeletar = int(input('Digite o ID do Produto que deseja excluir: \n'))
+
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('delete from produtos where id = {};'.format(idDeletar))
+            conexao.commit()
+    except:
+        print('Erro ao conectar ao banco de dados!!!')
+
+def listarPedidos():
+    pedidos = []
+    pedidoCliente = 0
+
+    while pedidoCliente != 2:
+        pedidos.clear()
+
+        try:
+            with conexao.cursor() as cursor:
+                cursor.execute('select * from pedidos;')
+                listaPedidos = cursor.fetchall()
+        except:
+            print()
+
+        for i in listaPedidos:
+            pedidos.append(i)
+
+        if len(pedidos) != 0:
+            for i in range(0, len(pedidos)):
+                print(pedidos[i])
+        else:
+            print('Nenhum pedido feito....')
+
+        pedidoCliente = int(input('Digite 1 para finalizar Pedido entregue, 2 para voltar'))
+
+        if pedidoCliente == 1:
+            idDeletar = int(input('Digite o ID do Pedido entregue: \n'))
+
+            try:
+                with conexao.cursor() as cursor:
+                    cursor.execute('delete from pedidos where id = {}'.format(idDeletar))
+                    print('Pedido entregue!!!')
+            except:
+                print('Erro ao conectar ao banco de dados para finalizar o pedido.')
+
+# GERANDO GRÁFICOS
+def gerarEstatistica():
+
+    nomeProdutos = []
+    nomeProdutos.clear()
+
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('select * from produtos;')
+            produtos = cursor.fetchall()
+    except:
+        print('Erro ao conectar ao banco de dados!!!')
+
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('select * from estatisticaVendido')
+            vendido = cursor.fetchall()
+    except:
+        print('Erro ao conectar ao banco de dados!!!')
+
+    estado = int(input('Digite 0 para sair, 1 para pesquisar por nome, 2 para pesquisar por grupo'))
+
+    if estado == 1:
+        decisao3 = int(input('Digite 1 para pesquisar por dinheiro, 2 para pesquisar por quantidade unitária'))
+        if decisao3 == 1:
+
+            for i in produtos:
+                nomeProdutos.append(i['nome'])
+
+                valores = []
+                valores.clear()
+
+                for h in range(0, len(nomeProdutos)):
+                    somaValor = -1
+                    for i in vendido:
+                        if i['nome'] == nomeProdutos[h]:
+                            somaValor += i['preco']
+                    if somaValor == -1:
+                            valores.append(0)
+                    elif somaValor >= 0:
+                        valores.append(somaValor+1)
+
+            plt.plot(nomeProdutos, valores)
+            plt.ylabel('Quantidade vendida em reais.')
+            plt.xlabel('Produtos')
+            plt.show()
+        if decisao3 == 2:
+            grupoUnico = []
+            grupoUnico.clear()
+
+            try:
+                with conexao.cursor() as cursor:
+                    cursor.execute('select * from produtos;')
+                    grupo = cursor.fetchall()
+            except:
+                print('Erro na consulta!!!')
+
+            try:
+                with conexao.cursor() as cursor:
+                    cursor.execute('select * from estatisticaVendido')
+                    vendidoGrupo = cursor.fetchall()
+            except:
+                print('Erro na consulta!!!')
+
+            for i in grupo:
+                grupoUnico.append(i['nome'])
+
+            #print(grupoUnico)
+            # verificar se tem elementos repetidos e apagar caso encontre algum
+            grupoUnico = sorted(set(grupoUnico))
+            quantFinal = []
+            quantFinal.clear()
+
+            for h in range(0, len(grupoUnico)):
+                quantUnitaria = 0
+                for i in vendidoGrupo:
+                    if grupoUnico[h] == i['nome']:
+                        quantUnitaria += 1
+                quantFinal.append(quantUnitaria)
+
+            plt.plot(grupoUnico, quantFinal)
+            plt.ylabel('Quantidade Unitária Vendida')
+            plt.xlabel('Produtos')
+            plt.show()
+
+    elif estado == 2:
+        decisao3 = int ( input ( 'Digite 1 para pesquisar por dinheiro, 2 para pesquisar por quantidade unitária' ) )
+        if decisao3 == 1:
+
+            for i in produtos:
+                nomeProdutos.append ( i['grupo'] )
+
+                valores = []
+                valores.clear ()
+
+                for h in range ( 0, len ( nomeProdutos ) ):
+                    somaValor = -1
+                    for i in vendido:
+                        if i['grupo'] == nomeProdutos[h]:
+                            somaValor += i['preco']
+                    if somaValor == -1:
+                        valores.append ( 0 )
+                    elif somaValor >= 0:
+                        valores.append ( somaValor + 1 )
+
+            plt.plot ( nomeProdutos, valores )
+            plt.ylabel ( 'Quantidade vendida em reais.' )
+            plt.xlabel ( 'Produtos' )
+            plt.show ()
+
+        if decisao3 == 2:
+            grupoUnico = []
+            grupoUnico.clear()
+
+            try:
+                with conexao.cursor() as cursor:
+                    cursor.execute('select * from produtos;')
+                    grupo = cursor.fetchall()
+            except:
+                print('Erro na consulta!!!')
+
+            try:
+                with conexao.cursor() as cursor:
+                    cursor.execute('select * from estatisticaVendido')
+                    vendidoGrupo = cursor.fetchall()
+            except:
+                print('Erro na consulta!!!')
+
+            for i in grupo:
+                grupoUnico.append(i['grupo'])
+
+            #print(grupoUnico)
+            # verificar se tem elementos repetidos e apagar caso encontre algum
+            grupoUnico = sorted(set(grupoUnico))
+            quantFinal = []
+            quantFinal.clear()
+
+            for h in range(0, len(grupoUnico)):
+                quantUnitaria = 0
+                for i in vendidoGrupo:
+                    if grupoUnico[h] == i['grupo']:
+                        quantUnitaria += 1
+                quantFinal.append(quantUnitaria)
+
+            plt.plot(grupoUnico, quantFinal)
+            plt.ylabel('Quantidade Unitária Vendida')
+            plt.xlabel('Produtos')
+            plt.show()
+
+
 while not autentico:
     decisao = int(input('Digite 1 para Logar e 2 para se Cadastrar'))
 
@@ -107,10 +303,22 @@ if autentico == True:
         decisaoUsuario = 1
 
         while decisaoUsuario != 0:
-            decisaoUsuario = int ( input ( 'Digite 0 para sair, 1 para cadastrar produtos, 2 para listar produtos cadastrados' ) )
+            decisaoUsuario = int ( input ( 'Digite 0 para sair, 1 para cadastrar produtos, 2 para listar produtos cadastrados, 3 listar Pedidos, 4 para visualizar as estatisticas') )
 
             if decisaoUsuario == 1:
                 cadastrarProdutos()
             elif decisaoUsuario == 2:
                 listarProdutos()
+
+                delete = int(input('Digite 1 para excluir Produto, 2 para sair'))
+                if delete == 1:
+                    excluirProdutos()
+                    print('Produto excluido com sucesso!!!')
+            elif decisaoUsuario == 3:
+                listarPedidos()
+            elif decisaoUsuario == 4:
+                gerarEstatistica()
+
+
+
 
